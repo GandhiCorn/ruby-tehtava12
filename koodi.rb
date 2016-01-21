0804522bc4c482f5fd92246e583f6f68663de9f8
Tuisku Haro <tuisku.haro@gmail.com>
2016-01-21 17:20:47 +0200

    luokittelu toimii

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,22 @@
 # kirjoita koodi tänne
+def luokittelu(taulu)
+	hash = {negatiivinen: [], positiivinen: [], parillinen: [], pariton: [], summa: 0}
+	taulu.each do |luku|
+		if luku < 0
+			hash [:negatiivinen].push(luku)
+		else 
+			hash[:positiivinen].push(luku)
+		end
+		if luku%2 == 0
+			hash[:parillinen].push(luku)
+		else
+			hash[:pariton].push(luku)
+		end
+		hash[:summa] += luku
+	end
+
+	hash.each_key do |avain|
+		hash[avain].sort! if hash[avain].kind_of? Array
+	end
+	return hash
+end
